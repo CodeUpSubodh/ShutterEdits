70b512f2d165f3c71c29718ed2ab11bfc85d1d3e
r-a-j-s-i-n-h-a <sinha.raj1236@gmail.com>
2023-06-25 22:50:39 +0530

    IssueResolved

--- ew.js
+++ ew.js
@@ -1,47 +1,198 @@
-const file = document.querySelector("#file")
-const download=document.getElementById("download")
-file.addEventListener("change", function() {
-  const reader = new FileReader()
-  reader.addEventListener("load", () => {
-    document.querySelector("#img").src = reader.result;
-     var original = reader.result;
-  })
-  reader.readAsDataURL(this.files[0]);
-})
-var image = document.querySelector('img');
-		var filterControls = document.querySelectorAll('input[type=range]');
+var image=null;
+var photo=null;
+const download = document.getElementById("download");
+let canvas =document.querySelector("canvas");
+const input = document.getElementById("img");
+const reader = new FileReader(); 
+
+
+input.addEventListener('change', () => {
+    reader.readAsDataURL(input.files[0]);
+});
+function upload() {
+  
+ var imgcanvas = document.getElementById("canvas");
+  var fileinput=document.getElementById("img");
+  image=new SimpleImage(fileinput);
+  photo=new SimpleImage(fileinput);
+ image.drawTo(imgcanvas); 
+
+}
+function makegray(){
+ 
+  for(var pixel of image.values())
+    {
+      var avg=(pixel.getRed()+pixel.getGreen()+pixel.getBlue())/3;
+      pixel.setRed(avg);
+      pixel.setBlue(avg);
+      pixel.setGreen(avg);
+    }
+      // var imgcanvas = document.getElementById("canvas");
+      // image.drawTo(imgcanvas);
+      // download.href = canvas.toDataURL();
+  
+}
+function remove(){
+  const context = canvas.getContext('2d');
+context.clearRect(0, 0, canvas.width, canvas.height);
+}
+function original(){
+   var imgcanvas= document.getElementById("canvas");
+  image=photo;
+      photo.drawTo(imgcanvas);
+}
+function expo(){
+   for(var pixel of image.values())
+     {
+       var g=pixel.getGreen();
+       var b=pixel.getBlue();
+       var r=pixel.getRed();
+       pixel.setGreen(g+5);
+       pixel.setBlue(b+5);
+       pixel.setRed(r+5);
+     }
+      var imgcanvas = document.getElementById("canvas");
+      image.drawTo(imgcanvas);
+      download.href = canvas.toDataURL();
+  
+  
+
+}
+function saturate(){
+   for(var pixel of image.values())
+     {
+       var g=pixel.getGreen();
+       var b=pixel.getBlue();
+       var r=pixel.getRed();
+       if(b>g && b>r)
+         {
+       pixel.setGreen(g-10);
+       pixel.setBlue(b+10);
+       pixel.setRed(r-10);
+         }
+       else if(g>b && g>r)
+         {
+       pixel.setGreen(g+10);
+       pixel.setBlue(b-10);
+       pixel.setRed(r-10);
+         }
+       else
+         {
+       pixel.setGreen(g-10);
+       pixel.setBlue(b-10);
+       pixel.setRed(r+10);
+         }
+     }
+      var imgcanvas = document.getElementById("canvas");
+      image.drawTo(imgcanvas);
+  
+  
+
+}
+function lowexpo(){
+   for(var pixel of image.values())
+     {
+       var g=pixel.getGreen();
+       var b=pixel.getBlue();
+       var r=pixel.getRed();
+       pixel.setGreen(g-5);
+       pixel.setBlue(b-5);
+       pixel.setRed(r-5);
+     }
+      var imgcanvas = document.getElementById("canvas");
+      image.drawTo(imgcanvas);
+  
+  
+
+}
+function dosaturate(){
+   var sizeinput=document.getElementById("saturateslider");
+   var size=sizeinput.value;
+  
+  for(var pixel of image.values())
+     {
+       var g=pixel.getGreen();
+       var b=pixel.getBlue();
+       var r=pixel.getRed();
+       if(b>g && b>r)
+         {
+       pixel.setGreen(g-3);
+       pixel.setBlue(b+3);
+       pixel.setRed(r-3);
+         }
+       else if(g>b && g>r)
+         {
+       pixel.setGreen(g+3);
+       pixel.setBlue(b-3);
+       pixel.setRed(r-3);
+         }
+       else
+         {
+       pixel.setGreen(g-3);
+       pixel.setBlue(b-3);
+       pixel.setRed(r+3);
+         }
+     }
+      var imgcanvas = document.getElementById("canvas");
+      image.drawTo(imgcanvas);
+      var current=size;
+  
+   
+   
+
+  
+
+}
+function dolowexpo(){
+  var sizeinput=document.getElementById("lowexposlider");
+   var size=sizeinput.value;
+   for(var pixel of image.values())
+     { 
+       var g=pixel.getGreen();
+       var b=pixel.getBlue();
+       var r=pixel.getRed();
+       pixel.setGreen(g-2*size);
+       pixel.setBlue(b-2*size);
+       pixel.setRed(r-2*size);
+     }
+  var imgcanvas = document.getElementById("canvas");
+      image.drawTo(imgcanvas);
+}
+function doexpo()
+{
+  var sizeinput=document.getElementById("exposlider");
+   var size=sizeinput.value;
+         for(var pixel of image.values())
+     {
+       var g=pixel.getGreen();
+       var b=pixel.getBlue();
+       var r=pixel.getRed();
+       pixel.setGreen(g+size/1.5);
+       pixel.setBlue(b+size/1.5);
+       pixel.setRed(r+size/1.5);
+     }
     
+      var imgcanvas = document.getElementById("canvas");
+      image.drawTo(imgcanvas);
+
+}
+
+		var filterControls = document.querySelectorAll('input[type=range]');
 		function applyFilter() {
 			var computedFilters = '';
 			filterControls.forEach(function(item, index) {
 				computedFilters += item.getAttribute('data-filter') + '(' + item.value + item.getAttribute('data-scale') + ') ';
 			});
-			image.style.filter = computedFilters;
       
-		};
-    const reader = new FileReader(); 
-reader.addEventListener('load', () => {
-    image.src = reader.result; 
-    originalData = reader.result;
-});
+      var imgcanvas = document.getElementById("canvas");
+      image.drawTo(imgcanvas);
 
-    
-    function originalData()
-    {
-      document.querySelector("#img").src = original;
-    }
-    function upload()
-    {
-      var def=document.querySelector("#img")
-    }
-    function refresh()
-    {
-      
-      document.getElementsById("blur").value=document.getElementById("blur").defaultValue;
-      var image = document.querySelector('img');
-      applyFilter();
-      console.log(document.getElementsById("blur").value);
-      
-      
-    }
-    
+		};
+
+    download.addEventListener("click",(e)=>{
+      let url=canvas.toDataURL();
+      let a=document.createElement("a");
+      a.href=url;
+      a.download="board.jpg";
+      a.click();
+   })
